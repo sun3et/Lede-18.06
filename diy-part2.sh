1119d90bd5c321ebbf0cd2f9d3f3d9e85e562f7c
Sunburns <72607204+Sunburns@users.noreply.github.com>
2021-08-31 21:27:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,4 @@
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-Argon_Blue/g' ./feeds/luci/collections/luci/Makefile
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$pNdVv1Pa$Oki2nWeW1x.08Pe4gB2/q/:18870:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
