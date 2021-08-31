9a0e4dbc6933fc22adf59e7f90372808ecb35153
Sunburns <72607204+Sunburns@users.noreply.github.com>
2021-08-31 21:49:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_blue/g' ./feeds/luci/collections/luci/Makefile
