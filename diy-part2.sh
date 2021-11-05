ed388f302e8362d3ec20d21fce832adebd50f354
sun3et <72607204+sun3et@users.noreply.github.com>
2021-11-05 14:37:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,4 +16,5 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 chmod -R 755 files/usr
 chmod -R 600 files/etc
+chmod -R 644 files/etc/config/AdGuardHome.yaml
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
