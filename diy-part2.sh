b64e93f34504d1df3e073da441851db3978eee80
sun3et <72607204+sun3et@users.noreply.github.com>
2021-11-06 15:27:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,5 @@
 #chmod -R 644 files/etc/config/AdGuardHome.yaml
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon_blue/g' ./feeds/luci/collections/luci/Makefile
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argonorigin
