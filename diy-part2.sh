57a554273ea20482a4212a9555b9112ab6d4cc94
sun3et <72607204+sun3et@users.noreply.github.com>
2021-11-06 16:36:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon_blue/g' ./feeds/luci/collections/luci/Makefile
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
