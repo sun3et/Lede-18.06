dbc04e6df7f2dca8941eb8cd81f74d02dacddc46
sun3et <72607204+sun3et@users.noreply.github.com>
2021-11-06 16:36:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-#chmod -R 755 files/usr
-#chmod -R 600 files/etc
-#chmod -R 644 files/etc/config/AdGuardHome
-#chmod -R 644 files/etc/config/AdGuardHome.yaml
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
