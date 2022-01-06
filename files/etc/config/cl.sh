03b7fd7134e9388d49e36094fbb1a324fa917cde
sun3et <72607204+sun3et@users.noreply.github.com>
2022-01-06 23:31:45 +0800

    Update cl.sh

--- files/etc/config/cl.sh
+++ files/etc/config/cl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm /etc/config/ChinaList.txt && wget -P /etc/config https://adguard.yojigen.tech/ChinaList.txt && sed -i '1i 208.67.222.222:5353' /etc/config/ChinaList.txt && sed -i '2i quic://i.passcloud.xyz:784' /etc/config/ChinaList.txt && sed -i '3i quic://x.passcloud.xyz:784' /etc/config/ChinaList.txt && /etc/init.d/AdGuardHome restart
+cd /etc/config/ && wget --no-check-certificate "https://adguard.yojigen.tech/ChinaList.txt" && rm ChinaList.txt && mv ChinaList.txt.1 ChinaList.txt && sed -i '1i 208.67.222.222:5353' ChinaList.txt && sed -i '2i quic://i.passcloud.xyz:784' ChinaList.txt && sed -i '3i quic://x.passcloud.xyz:784' ChinaList.txt && /etc/init.d/AdGuardHome restart
