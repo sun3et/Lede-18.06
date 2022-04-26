f8bbea28200c20f221e91776404f5c3fb5e9efbe
sun3et <72607204+sun3et@users.noreply.github.com>
2022-04-27 01:05:22 +0800

    Update cl.sh

--- files/etc/config/cl.sh
+++ files/etc/config/cl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /etc/
 wget --no-check-certificate "https://adguard.yojigen.tech/ChinaList.txt" &&
-sed -i '1i 127.0.0.1:7913' ChinaList.txt &&
+sed -i '1i 127.0.0.1:7914' ChinaList.txt &&
 mv /etc/ChinaList.txt /etc/config/cl.txt &&
 /etc/init.d/AdGuardHome restart
